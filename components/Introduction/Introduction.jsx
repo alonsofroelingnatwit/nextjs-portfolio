dd15d32a752f2270ef037b5daffb5a2bd6f434bf
Nicolas Alonso Froeling <nicolasalonsofroeling@Nicolass-MacBook-Pro.local>
2022-10-12 01:19:51 -0400

    added text in the about

--- components/Introduction/Introduction.jsx
+++ components/Introduction/Introduction.jsx
@@ -6,10 +6,10 @@ const Introduction = () => {
       <h1 className={styles.introduction_title}>Hello everyone, I am</h1>
       <h1 className={styles.introduction_name}>Nicolas Alonso Froeling</h1>
       <p className={styles.introduction_text}>
-        I am a computer science student at Wentworth Institute of Technology. I
-        am currently looking for a full-time position as a software engineer.
-        lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        I am a computer science student at Wentworth Institute of Technology.
+      </p>
+      <p className={styles.introduction_under_text}>
+        From Madrid, Spain. based in Boston.
       </p>
     </div>
   );
--- components/about/About.jsx
+++ components/about/About.jsx
@@ -4,6 +4,18 @@ const About = () => {
   return (
     <div className={styles.about_about}>
       <h1 className={styles.about_title}>About</h1>
+      <p className={styles.about_text}>
+        Hello! I am Nicolas Alonso Froeling, I am from Madrid, Spain and
+        currently based in Boston. I am passionate about technology and I am
+        always looking for new opportunities to learn and grow.
+      </p>
+      <p className={styles.about_text}>
+        I am fluent in Spanish, English, Dutch and I speak basic German.
+      </p>
+      <p className={styles.about_text}>
+        I love sports! I am an athlete for the Wentworth golf team, I am the
+        upcoming captain. I always try to be a leader and show by example.
+      </p>
     </div>
   );
 };
