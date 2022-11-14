ae8002a0b61648af03227df7658a1af365d6ae1f
Nicolas Alonso Froeling <nicolasalonsofroeling@Nicolass-MacBook-Pro.local>
2022-11-14 01:49:43 -0500

    setting the font to Montserrat and adding a nice background

--- components/Introduction/Introduction.jsx
+++ components/Introduction/Introduction.jsx
@@ -3,13 +3,12 @@ import styles from "./Introduction.module.css";
 const Introduction = () => {
   return (
     <div className={styles.introduction}>
-      <h1 className={styles.introduction_title}>Hello everyone, I am</h1>
       <h1 className={styles.introduction_name}>Nicolas Alonso Froeling</h1>
       <p className={styles.introduction_text}>
-        I am a computer science student at Wentworth Institute of Technology.
+        Computer science student at Wentworth Institute of Technology
       </p>
       <p className={styles.introduction_under_text}>
-        From Madrid, Spain. based in Boston.
+        From Madrid, Spain. Based in Boston.
       </p>
     </div>
   );
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -4,7 +4,6 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <header className={styles.header_header}>
-      <img className={styles.Header_logo} src="/NA-logo.svg" alt="NA logo" />
       <ul className={styles.header_list}>
         <li className={styles.header_list_item}>Home</li>
         <li className={styles.header_list_item}>About</li>
