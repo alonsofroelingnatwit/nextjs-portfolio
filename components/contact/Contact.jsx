2e344d3af531bf171f980a2f3d3e3b36110a1fea
Nicolas Alonso Froeling <nicolasalonsofroeling@Nicolass-MacBook-Pro.local>
2023-02-15 16:32:06 -0500

    contact form design

--- components/contact/Contact.jsx
+++ components/contact/Contact.jsx
@@ -8,16 +8,35 @@ const Contact = () => {
         If you have any questions or would like to work together, please feel
         free to contact me!
       </p>
-      <p className={styles.contact_text}>
-        Email:
-        <a
-          href="mailto: nicolas.alonsofroeling@gmail.com"
-          className={styles.contact_text_link}
-        >
-          {" "}
-          hello
-        </a>
-      </p>
+      <div className={styles.contact_form}>
+        <form>
+          <div className={styles.contact_form_group}>
+            <input
+              type="text"
+              id="name"
+              className={styles.contact_form_group_input}
+              placeholder="Name"
+            />
+            <hr className={styles.contact_form_line} />
+            <input
+              type="email"
+              id="email"
+              className={styles.contact_form_group_input}
+              placeholder="Email"
+            />
+            <hr className={styles.contact_form_line} />
+            <textarea
+              id="message"
+              rows="5"
+              className={styles.contact_form_group_textarea}
+              placeholder="Message"
+            />
+            <button type="submit" className={styles.contact_form_group_button}>
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
